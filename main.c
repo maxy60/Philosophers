505eeef35828e95f6d7c9f91207e31aaa4b7e065
maxy60 <cest-mon-yop@hotmail.fr>
2022-04-22 11:43:23 +0200

    patch double wait philo

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: msainton <msainton@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/28 15:23:58 by msainton          #+#    #+#             */
-/*   Updated: 2022/04/15 16:41:39 by msainton         ###   ########.fr       */
+/*   Updated: 2022/04/22 11:34:43 by msainton         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,8 @@ int	main(int argc, char **argv)
 		create_threads_pair(info.philo, info.n_philo);
 		check_death(&info);
 		pthread_mutex_destroy(&info.meal);
-		join_mythread(info.philo, info.n_philo);
+		//if (info.philo)
+		//	join_mythread(info.philo, info.n_philo);
 		free(info.philo);
 	}
 	else
